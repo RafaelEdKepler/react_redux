31a17e377e0f8eebb3d0317343c9c3dae40a744b
Rafael Eduardo Kepler <rafael.kepler@hotmail.com>
2021-10-01 09:31:05 -0300

    Aula Trabalhando com Actions - Finalizada

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,9 +1,39 @@
-import { useSelector } from "react-redux"
+import { useEffect, useState, useCallback } from "react"
+import { useDispatch } from "react-redux"
+import api from "../services/api";
+import { addProductToCart } from "../store/modules/cart/actions";
+import { IProduct } from "../store/modules/cart/types";
 
 export default function Catalog() {
-  const catalog = useSelector(state => state);
+  const dispatch = useDispatch();
+  const [ catalog, setCatalog ] = useState<IProduct[]>([]);
+
+  useEffect(() => {
+    api.get('products').then(response => {
+      setCatalog(response.data);
+    })
+  }, []);
+
+  const handleAddProductToCart = useCallback((product) => {
+    dispatch(addProductToCart(product))
+  }, [dispatch]);
 
   return (
-    <h1>Catalog</h1>
+    <>
+      <h1>Catalog</h1>
+
+      {catalog && catalog.map(item => (
+        <article key={item.id}>
+          <strong>{item.title}</strong> {" - "}
+          <span>{item.price}</span> {" "}
+          <button
+           type="button"
+           onClick={(item) => handleAddProductToCart(item)}
+          >
+            Comprar
+          </button>
+        </article>
+      ))}
+    </>
   )
 }
